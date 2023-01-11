15c764724dd32207a2c0403366a6354abe20ebf3
Finn Ganser <38922171+VogelPapaFinn@users.noreply.github.com>
2023-01-11 11:17:40 +0100

    added list support

--- config_file.cpp
+++ config_file.cpp
@@ -1,11 +1,11 @@
-﻿#include "config_file.h"
+#include "config_file.h"
 
 config_file::config_file(const std::string& file)
 {
 	file_ = file;
 
 	line_vector_ = {""};
-	
+
 	// Open config file
 	fstream_.open(file, std::fstream::in);
 
@@ -49,7 +49,7 @@ bool config_file::file_valid(const std::string& file)
 
 	// really basic DEA
 	std::vector<char> basic_dea;
-	
+
 	// Check all lines
 	for(auto& l : line_vector)
 	{
@@ -73,7 +73,7 @@ bool config_file::file_valid(const std::string& file)
 
 		// Attribute without group - ERROR
 		else if(l.find(':') == std::string::npos && !l.empty())
-			basic_dea.push_back('E');  
+			basic_dea.push_back('E');
 
 		// Attribute
 		else if(l.find(':') != std::string::npos && basic_dea.empty())
@@ -115,7 +115,7 @@ void config_file::save()
 	// Reopen config to write
 	fstream_.close();
 	fstream_.open(file_, std::ios::out | std::ios::trunc);
-	
+
 	// Push all lines
 	for (std::string& s : line_vector_)
 	{
@@ -200,6 +200,54 @@ bool config_file::write(const std::string& name, const std::string& value, const
 	return false;
 }
 
+bool config_file::write_list(const std::string &name, const std::vector<std::string> &value)
+{
+    // Is the config file opened?
+    if (current_state_ == state::closed)
+        return false; // No
+
+    // return
+    return this->write_list(name, value, "default");
+}
+
+bool config_file::write_list(const std::string& name, const std::vector<std::string>& value, const std::string& group)
+{
+    // Is the config file opened?
+    if (current_state_ == state::closed)
+        return false; // No
+
+    // Does the attribute in the group exist?
+    int line = this->find(name, group);
+    if (line != -1)
+        // Yes, replace line
+        line_vector_.at(line).replace(name.size() + 3, line_vector_.at(line).size() - (name.size() + 3), vector_to_string(value));
+    else // No
+    {
+        // Does the group exist?
+        line = this->find(group);
+        const std::string tmp = name + ":/l" + vector_to_string(value);
+        if (line != -1)
+        {
+            // Insert new line
+            line_vector_.insert(line_vector_.begin() + line + 1, tmp);
+
+            return true;
+        }
+        else
+        {
+            // Create new group
+            line_vector_.insert(line_vector_.end(), "[" + group + "]");
+            line_vector_.insert(line_vector_.end(), tmp);
+            line_vector_.insert(line_vector_.end(), "[/" + group + "]");
+            line_vector_.insert(line_vector_.end(), "");
+
+            return true;
+        }
+    }
+
+    // Didnt work
+    return false;
+}
 
 
 std::optional<std::string> config_file::get(const std::string& name) const
@@ -212,10 +260,63 @@ std::optional<std::string> config_file::get(const std::string& name, const std::
 	// Does attribute and group exist?
 	const int i = this->find(name, group);
 	if (i == -1) return std::nullopt; // No
-	
+
 	// Yes return value
 	return line_vector_.at(i).substr(name.size() + 3, line_vector_.at(i).size() - (name.size() + 3));
 }
+
+std::vector<std::string> config_file::get_list(const std::string& name) const
+{
+    return get_list(name, "default");
+}
+
+std::vector<std::string> config_file::get_list(const std::string& name, const std::string& group) const
+{
+    // Does attribute and group exist?
+    const int line = this->find(name, group);
+    if (line == -1) return std::vector<std::string>(); // No
+
+    // Save string to parse
+    std::string unparsed_list = line_vector_.at(line).substr(name.size() + 3, line_vector_.at(line).size() - (name.size() + 3));
+
+    // Parse string
+    // { list head
+    // } list tail
+    // , new list element
+    std::vector<std::string> output;
+    std::string element = "";
+    bool list_opened = false;
+    for(auto& c : unparsed_list)
+    {
+        switch(c)
+        {
+            case '{': list_opened = true; break;
+            case '}': list_opened = false; output.push_back(element); break;
+            case ',': output.push_back(element); element = ""; break;
+            default: element += c;
+        }
+    }
+
+    return output;
+}
+
+std::optional<std::string> config_file::get_raw_list(const std::string& name) const
+{
+    return get_raw_list(name, "default");
+}
+
+std::optional<std::string> config_file::get_raw_list(const std::string& name, const std::string& group) const
+{
+    // Does attribute and group exist?
+    const int line = this->find(name, group);
+    if (line == -1) return std::nullopt;
+
+    // Save string to parse
+    std::string unparsed_list = line_vector_.at(line).substr(name.size() + 3, line_vector_.at(line).size() - (name.size() + 3));
+
+    return unparsed_list;
+}
+
 
 
 
@@ -226,12 +327,6 @@ void config_file::remove(const std::string& name, const std::string& group)
 	if (i != -1)
 		// Delete line
 		line_vector_.erase(line_vector_.begin() + i, line_vector_.begin() + i + 1);
-}
-
-void config_file::remove(const std::string& name, const char* group)
-{
-	// remove
-	this->remove(name, std::string(group));
 }
 
 void config_file::remove(const std::string& name)
@@ -249,7 +344,7 @@ void config_file::remove(const std::string& group, const bool move)
 	if (i != -1) // Yes
 	{
 		// Move everything to default?
-		if (move) 
+		if (move)
 		{
 			// Where is the default group?
 			const int d = this->find("default");
@@ -313,17 +408,14 @@ void config_file::move(const std::string& name, const std::string& new_group)
 
 
 
-int config_file::find(const std::string_view group) const
+int config_file::find(const std::string& group) const
 {
-	auto const itr = std::ranges::find_if(line_vector_, [&](const std::string_view s)
-	{
-		return s.starts_with('[') && s.ends_with(']') && s.substr(1, s.size() - 2) == group;
-	});
+    // Search all lines
+    for(int l = 0; l < line_vector_.size(); l++)
+        if(line_vector_[l].front() == '[' && line_vector_[l].back() == ']' && line_vector_[l].substr(1, line_vector_[l].size() - 2) == group)
+            return l;   // Found
 
-	// Did it find something?
-	if (itr != line_vector_.end())
-		return std::distance(std::begin(line_vector_), itr);	// Yes, return the distance/line number
-	return -1;	// Nope, return nothing
+	return -1;	// Not found, return nothing
 }
 
 int config_file::find(const std::string& name, const std::string& group) const
@@ -369,11 +461,23 @@ void config_file::read_lines()
 
 
 
+std::string config_file::vector_to_string(const std::vector<std::string>& vector)
+{
+    std::string output = "{";
+
+    for(auto& l : vector)
+        output += l + ",";
+
+    return output.substr(0, output.size() - 1) + "}";
+}
+
+
+
 std::string config_file::translate_code(const std::string& line)
 {
 	std::string end; // return string
 
-	/* 
+	/*
 	* space		   : /l
 	* '#'          : gets deleted
 	* ':'		   : stays
@@ -422,7 +526,9 @@ std::string config_file::translate_file(const std::string& line)
 void config_file::smooth()
 {
 	// Delete all empty lines
-	std::erase(line_vector_, "");
+	for(auto itr = line_vector_.begin(); itr != line_vector_.end(); itr++)
+        if(*itr == "")
+            itr = line_vector_.erase(itr);
 
 	// Insert empty line after every group [/XXX]
 	for (int i = 0; i < line_vector_.size() && line_vector_.at(i) != line_vector_.back(); i++)
